35233f7c355b7d5cfce1695b2eb1807f8c20703c
BBD <741414851@qq.com>
2016-08-17 10:17:32 +0800

    添加Image

---
+++ BBDCounter/BBDTimerView.h
@@ -0,0 +1,10 @@
+
+
+#import <UIKit/UIKit.h>
+
+@interface BBDTimerView : UIButton
+
+// 构造方法
+- (instancetype)initWithFrame:(CGRect)frame second:(NSInteger)second lineWidth:(int)lineWidth lineColor:(UIColor *)color font:(UIFont *)font;
+
+@end
